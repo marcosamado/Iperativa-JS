c6c1706fb7fbb11582d275d447b91aeb4ada0e3b
marcosamado <led_9dejulio@hotmail.com>
2022-08-15 18:44:55 -0300

    practica if / else}

--- calculadora.js
+++ calculadora.js
@@ -26,21 +26,41 @@ let dividir = (a,b) => a / b;
 // console.log(multiplicacion);
 
 // console.log(division);
-let cuadradoDeUnNumero = (numero) => multiplicar(numero,numero);
-console.log("el cuadrado de 2 es "+ cuadradoDeUnNumero(2));
+// let cuadradoDeUnNumero = (numero) => multiplicar(numero,numero);
+// console.log("el cuadrado de 2 es "+ cuadradoDeUnNumero(2));
 
-let promedioDeTresNumeros = (numero,numero2,numero3) => dividir((suma(numero,numero2) + numero3),3);
-console.log("el promedio es  "+ promedioDeTresNumeros(10,8,7.6));
+// let promedioDeTresNumeros = (numero,numero2,numero3) => dividir((suma(numero,numero2) + numero3),3);
+// console.log("el promedio es  "+ promedioDeTresNumeros(10,8,7.6));
 
-let calcularPorcentaje = (numero,porcentaje) => dividir(multiplicar(numero,porcentaje),100);
-console.log("el porcentaje es " + calcularPorcentaje(50,10));
-
-
-let generadorDePorcentaje = (difporcentaje,numero) => dividir(multiplicar(difporcentaje,100),numero);
-console.log("el porcentaje generado es " + generadorDePorcentaje(45,150));
+// let calcularPorcentaje = (numero,porcentaje) => dividir(multiplicar(numero,porcentaje),100);
+// console.log("el porcentaje es " + calcularPorcentaje(50,10));
 
 
+// let generadorDePorcentaje = (difporcentaje,numero) => dividir(multiplicar(difporcentaje,100),numero);
+// console.log("el porcentaje generado es " + generadorDePorcentaje(45,150));
 
+
+// SACAR PROMEDIO GENERAL
+// function calcularNota(nota1,nota2,nota3) {
+//     let resultado = (nota1 + nota2 + nota3)/ 3;
+//     if ( resultado > 6) {
+//         console.log("APROBAASTE!!!! " + resultado);
+//     }else {
+//         console.log("Te veo en diciembre amigo " + resultado);
+//     };
+// }
+// calcularNota (parseInt(prompt("ingresa nota parcial :")),parseInt(prompt("ingresa nota integrador :")),parseInt(prompt("ingrese nota concepto :")));
+
+
+// hacer una funcion que me reciba un numero si es par y menor a 6 que muestre el mensaje "el numero es par y menor a 6" en caso contrario "una de la condiciones no se cumple";
+
+function problemaRosita(num) {
+    if(num < 6 && num % 2 == 0){
+        console.log("El numero es par y menor a 6");
+    }else {
+        console.log("una de las condiciones no se cumple");
+    }
+};
 
 
 
