3bb1cabaf2fc337e3256fd1aec86ef3e20606888
marcosamado <led_9dejulio@hotmail.com>
2022-08-26 17:45:29 -0300

    criba de derat etc

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,18 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 let fin = Number(prompt("Ingrese cuántos valores de la serie Fibonacci desea visualizar: "))
-function fibonacci(fin){
+function fibonacci(x){
     let num1 = 0;
     let num2 = 1;
-    if (fin <= 0 || fin != NaN) {
+    if (x <= 0) {
         console.log("Demasiado chiquito tu numerito, probá mayor a 1 ;)");
     }
-    else if (fin === 1) {
+    else if (x === 1) {
         console.log(num1);
     } else {
         console.log(num1);
         console.log (num2);
-            for (let i=2 ; i<fin ; i++) {
+            for (let i=2 ; i<x ; i++) {
             let num3 = num1 + num2;
             console.log(num3);
             num1 = num2;
