1d85c34d8e0b00327db0da22374f92bca838842c
marcosamado <led_9dejulio@hotmail.com>
2022-08-31 21:15:41 -0300

    ejercitacion clase 11 con objetos literales en progreso

---
+++ ejercitacionclase11.js
@@ -0,0 +1,97 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// Desde un banco nos contactan para crear una aplicación que pueda facilitar el manejo
+// de información y las acciones que el mismo necesita. Nuestro tech leader nos facilita la
+// forma de representar a los usuarios (cuentas bancarias). Cada una de estas cuentas
+// tiene la siguiente información:
+// ● N.o de cuenta (solo números).
+// ● Tipo de cuenta (cuenta corriente en pesos o caja de ahorro en
+// pesos).
+// ● Saldo en pesos (solo la cantidad).
+// ● Titular de la cuenta (nombre completo).
+//la lista de clientes.
+let arrayCuentas = [
+    {
+    nroCuenta: 5486273622,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 27771,
+    titularCuenta: "Abigael Natte",
+    },
+    {
+    nroCuenta: 1183971869,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 8675,
+    titularCuenta: "Ramon Connell",
+    },
+    {
+    nroCuenta: 9582019689,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 27363,
+    titularCuenta: "Jarret Lafuente",
+    },
+    {
+    nroCuenta: 1761924656,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 32415,
+    titularCuenta: "Ansel Ardley",
+    },
+    {
+    nroCuenta: 7401971607,
+    tipoDeCuenta: "Cuenta Unica",
+    titularCuenta: "Jacki Shurmer",
+    saldoEnPesos: 18789,
+    },
+];
+// Nos solicitan también crear un objeto literal llamado “banco”, el cual tendrá una
+// propiedad llamada “clientes” que estará compuesta de la lista de objetos
+// obtenidos en el punto anterior.
+
+let banco = {
+    clientes : [
+        {
+        nroCuenta: 5486273622,
+        tipoDeCuenta: "Cuenta Corriente",
+        saldoEnPesos: 27771,
+        titularCuenta: "Abigael Natte",
+        },
+        {
+        nroCuenta: 1183971869,
+        tipoDeCuenta: "Caja de Ahorro",
+        saldoEnPesos: 8675,
+        titularCuenta: "Ramon Connell",
+        },
+        {
+        nroCuenta: 9582019689,
+        tipoDeCuenta: "Caja de Ahorro",
+        saldoEnPesos: 27363,
+        titularCuenta: "Jarret Lafuente",
+        },
+        {
+        nroCuenta: 1761924656,
+        tipoDeCuenta: "Cuenta Corriente",
+        saldoEnPesos: 32415,
+        titularCuenta: "Ansel Ardley",
+        },
+        {
+        nroCuenta: 7401971607,
+        tipoDeCuenta: "Cuenta Unica",
+        saldoEnPesos: 18789,
+        titularCuenta: "Jacki Shurmer",
+        }
+    ],
+    consultarCliente : function (nombre){
+        for (let i = 0; i <this.clientes.length; i++){
+            if(this.clientes.titularCuenta == nombre) {
+                return "encontrado";
+            }else{
+                return "No existe el cliente"
+            }
+        }
+        }
+    }
+// Al objeto “banco”, crearle un método llamado consultarCliente, el cual recibirá un
+// nombre (titular) por parámetro, deberá buscarlo en la lista de cuentas y retornar el
+// objeto cliente que corresponda con ese nombre ingresado.
+// Ayuda: let clienteEncontrado = banco.consultarCliente(“Ramon Connell”);
+
+console.log(banco.consultarCliente("Jacki Shurmer"));
--- objetosliterales.js
+++ objetosliterales.js
@@ -3,9 +3,17 @@ const prompt = require("prompt-sync")({ sigint: true });
 let curso = {
     cantidadAlumnos : 32,
     profesores : ["juan", "funcion"],
-    funcion : function () {
-        return "soy una " + this.profesores[1] + " dentro de un objeto";
+    funcion : function (nombre) {
+        for (let i = 0; i < this.profesores.length; i++){
+            this.profesores[i];
+            if(this.profesores[i] == nombre){
+                return curso
+            }else {
+                "no encontrado"
+            }
+        }
     }
 }
-console.log(curso.funcion());
+let clienteEncontrado = curso.funcion("funcion");
+console.log(clienteEncontrado);
 
