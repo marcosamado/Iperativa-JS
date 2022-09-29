9efa73a172f3c9a9e0b944a5d3394346cdf2ae19
marcosamado <led_9dejulio@hotmail.com>
2022-09-28 21:10:29 -0300

    coderWars ejercitacion

--- prueba.js
+++ prueba.js
@@ -1,6 +1,21 @@
-if (true) {
-    let nombre = "marcos";
-    console.log (nombre);
-}
-let nombre = "chonejo";
-console.log (nombre);
+// 39 >>>>  3x9 >> 27 >> 2x7 >> 14 >> 1x4 >>>> 4 
+
+let numero = 35112;
+
+function separando(num){
+    let cifras = []
+    while (num > 1){
+        cifras.push(parseInt(num % 10));
+        num = num/10;
+    }
+    return cifras;
+};
+
+console.log(separando(numero));
+
+let palabra = ['hello', 'world', 'this', 'is', 'great']
+
+function smash (words) {
+    return words.join(" ");
+};
+console.log(smash(palabra))
